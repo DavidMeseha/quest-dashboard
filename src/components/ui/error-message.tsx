acdf3bc6721a4e056768268522d43497a259c827
David <davidmmyh@gmail.com>
2025-09-10 21:06:32 +0300

    fix: error message hight

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -4,7 +4,7 @@ type Props = { error?: string | null };
 
 export default function ErrorMessage({ error }: Props) {
   return (
-    <p className="text-destructive mt-1 h-4 text-sm">
+    <p className="text-destructive mt-1 min-h-4 text-sm">
       {error && (
         <span className="flex items-center gap-1 font-semibold">
           <BiSolidErrorCircle className="inline-block" /> <span>{error}</span>
